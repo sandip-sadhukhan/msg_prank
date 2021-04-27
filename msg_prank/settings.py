0a996ce34a2bf25908ef9764a2a3cc9cd11afae2
SANDIP SADHUKHAN <sandippakhanna@gmail.com>
2021-04-27 22:06:55 +0530

    Added templates

--- msg_prank/settings.py
+++ msg_prank/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website.apps.WebsiteConfig',
 ]
 
 MIDDLEWARE = [
@@ -108,6 +109,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
-)
+]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def index(request):
+    if request.user.is_authenticated :
+        return redirect('inbox')
+    return render(request, 'website/pages/index.html')
+
+@login_required
+def inbox(request):
+    return render(request, 'website/pages/inbox.html')
